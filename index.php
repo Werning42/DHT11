793e3d83a0a94ddbf886adf7352b87d47565dd19
Pierrick Marconnet <tes@test.com>
2018-11-30 10:21:11 +0100

    Création dossier et fichier

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 echo('Hello World');
 
 ?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thermomètre</title>
+</head>
+<body>
+	<h1>Températures</h1>
+	<img src="img/thermometer.jpg">
+	<div></div>
+
+</body>
+
+</html>
